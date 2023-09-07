f664ab30f0f4399c2190fd7dee6e6b37848111b7
patorma <pocontreras@ing.ucsc.cl>
2023-09-06 23:01:37 -0300

    Se vio las directivas de *ng-if *ng-for

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  template: `
+  <h3>Counter: {{counter}}</h3>
+
+  <button (click)="increaseBy(+1)">+1</button>
+  <button (click)="reset(10)">Reset</button>
+  <button (click)="increaseBy(-1)">-1</button>
+
+  `
+})
+
+export class CounterComponent  {
+  public counter: number =10;
+  constructor() { }
+
+  increaseBy(value: number): void{
+    this.counter += value
+  }
+  reset(num: number) :void{
+    this.counter =num
+   }
+
+}
